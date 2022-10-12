f190d90cc226af0c64f91dd5a5496d9d4a9d1807
Lilou lilou <lidou34@live.fr>
2022-10-12 11:28:36 +0200

    avancement front pause pour back

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -10,51 +10,55 @@ const Login = () => {
 
   return (
     <div className="container-login">
-      <img
-        src={image}
-        alt="Homme debout dans une tour de controle vide"
-        className="img-login"
-      />
-      <div className="box-login">
-        <Logo className="logo" />
-        <form id="form-login" method="post" action="#">
-          <h1>Bienvenue sur votre réseau social d'entreprise</h1>
-          <p>Connectez-vous</p>
-          <div className="form-group">
-            <label htmlFor="email">Adresse email</label>
-            <input
-              className="name form-control"
-              type="email"
-              placeholder="exemple: dupont@gmail.com"
-              required
-              autoFocus
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Mot de passe</label>
-            <input
-              className="password form-control"
-              id="password"
-              type="text"
-              placeholder="exemple: Motdepasse01"
-              required
-            />
-            <button id="btnEye">
-              <i className="fas fa-eye" id="eye"></i>
+      <section className="imageLogin">
+        <img
+          src={image}
+          alt="Homme debout dans une tour de controle vide"
+          className="img-login"
+        />
+      </section>
+      <section className="box-login">
+        <div className="box-login-container ">
+          <Logo className="logo" />
+          <form id="form-login" method="post" action="#">
+            <h1>Bienvenue sur votre réseau social d'entreprise</h1>
+            <p>Connectez-vous</p>
+            <div className="form-group">
+              <label htmlFor="email">Adresse email</label>
+              <input
+                className="name form-control"
+                type="email"
+                placeholder="exemple: dupont@gmail.com"
+                required
+                autoFocus
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Mot de passe</label>
+              <input
+                className="password form-control"
+                id="password"
+                type="text"
+                placeholder="exemple: Motdepasse01"
+                required
+              />
+              <button id="btnEye">
+                <i className="fas fa-eye" id="eye"></i>
+              </button>
+            </div>
+            <button className="btn" type="submit">
+              <NavLink to="/" className="texte">
+                Connexion
+              </NavLink>
             </button>
-          </div>
-          <button className="btn" type="submit">
-            <NavLink to="/" className="texte">
-              Connexion
+          </form>
+          <p>
+            <NavLink to="/Signup" className="lien-signup">
+              Pas de compte ? Cliquez ici pour créer un compte
             </NavLink>
-          </button>
-        </form>
-        <p>
-          <NavLink to="/Signup" className="lien-signup">
-            Pas de compte ? Cliquez ici pour créer un compte
-          </NavLink>
-        </p>
-      </div>
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
