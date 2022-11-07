21fadb552dc1ede3b8382338f4f45101a0a24696
Lilou lilou <lidou34@live.fr>
2022-11-07 11:27:08 +0100

    erreur 404

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -40,44 +40,74 @@ schemaMDP
 
 /////////////////////////enregistrement nouvel user/////////////////////
 exports.signup = async (req, res, next) => {
- // const emailCrypt = cryptoJs
-    //.HmacSHA256(req.body.email, `${process.env.CLE_EMAIL}`)
-    //.toString(); //crypt email
-
+  /*const emailCrypt = cryptoJs
+  .HmacSHA256(req.body.email, `${process.env.CLE_EMAIL}`)
+  .toString(); //crypt email*/
   if (!emailValidator.validate(req.body.email)) {
     return res.status(400).json({ message: "Adresse email invalide !" });
-  } else if (!schemaMDP.validate(req.body.password)) {
-    return res.status(400).json({ message: "Mot de passe invalide !" });
-  } else {
-    //////////// Crypter le mot de passe////////////
-    bcrypt
-      .hash(req.body.password, 10) //bcrypt hash le mdp saler 10 fois
-      .then((hash) => {
-        console.log("creation user");
-        prisma.user
-          .create({
-            data: {
-              email: req.body.email,//emailCrypt,
-              password: hash,
-            },
-          })
-          .then((data) => {
-            console.log(data)
-            next()
-          })
-      })
-      .catch((error) => res.status(500).json({ error }));
   }
+  if (!schemaMDP.validate(req.body.password)) {
+    return res.status(401).json({ message: "Mot de passe invalide !" });
+  }
+  const emailFindUnique = await prisma.user.findUnique({
+    where: { where: { email: req.body.email }  },
+  });
+  
+  if (emailFindUnique) {
+    return res.status(402).json({ message: "Un compte existe déjà avec cette adresse mail, merci de vous connecter !" });
+  };
+  const hash = await bcrypt.hash(req.body.password, 10);
+  console.log(hash);
+    console.log("creation");
+  try {
+    if (!emailFindUnique) {
+      await prisma.user.create({
+        data: {
+          email: req.body.email,
+          password: hash,
+        },
+      });
+      
+    }
+    return next();
+    console.log("user created"); 
+    
+  } catch (error) {
+    res.status(500).json({ message: "enregistrement echoué" });
+  }
+  
 };
 
 ///////////////////////////conexion user avec compte//////////////////////////
 exports.login = async (req, res, next) => {
   //const emailCrypt = cryptoJs
-    //.HmacSHA256(req.body.email, `${process.env.CLE_EMAIL}`)
-    //.toString(); //crypt email
-  prisma.user
-    .findUnique({where: {email: req.body.email,}})
-    .then((user) => {
+  //.HmacSHA256(req.body.email, `${process.env.CLE_EMAIL}`)
+  //.toString(); //crypt email
+
+  try {
+    const user = prisma.user.findUnique({ where: { email: req.body.email } });
+    if (!user) {
+      res.status(401).json({ message: "Paire login/mot de passe incorrecte" });
+    }
+    const valid = await bcrypt.compare(req.body.password, user.password); //fonction compare de bcrypt pour comparer le mot de passe entré par l'user avec le hash enregistré dans la base de données
+
+    if (!valid) {
+      res.status(402).json({ message: "Paire login/mot de passe incorrecte" });
+    }
+    //Nous renvoyons le token au front-end avec notre réponse.
+    return res.status(200).json({
+      token: jwt.sign(
+        //fonction sign de jsonwebtoken pour chiffrer un nouveau token
+        { userId: user.id },
+        process.env.TOKEN_SECRET, // chiffrage secret
+        { expiresIn: "24h" } //durée de validité du token à 24 heures
+      ),
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+/*.then((user) => {
       console.log(user);
       if (!user) {
         return res
@@ -106,5 +136,4 @@ exports.login = async (req, res, next) => {
         })
         .catch((error) => res.status(501).json({ error }));
     })
-    .catch((error) => res.status(500).json({ error }));
-};
+    .catch((error) => res.status(500).json({ error }));*/
